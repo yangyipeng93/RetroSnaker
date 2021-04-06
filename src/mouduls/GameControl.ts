ca2631fb0d604e4d6e14f44d1e761793c1abb080
yangyipeng <578705414@qq.com>
2021-04-06 22:43:31 +0800

    update

--- src/mouduls/GameControl.ts
+++ src/mouduls/GameControl.ts
@@ -5,39 +5,41 @@ import ScorePanel from "./ScorePanel";
 //游戏控制器
 export default class GameControl {
 
-    snake:Snake;
+    snake: Snake;
 
-    food:Food;
+    food: Food;
 
-    scorePanel:ScorePanel;
+    scorePanel: ScorePanel;
 
-    direction:string='';
+    direction: string = '';
+
+    isLive: boolean = true;
 
 
     constructor() {
-        this.snake=new Snake();
-        this.food=new Food();
-        this.scorePanel=new ScorePanel();
+        this.snake = new Snake();
+        this.food = new Food();
+        this.scorePanel = new ScorePanel();
 
         this.init();
     }
 
     // 游戏的初始化方法，调用后游戏即开始
-    init(){
+    init() {
         // 绑定键盘按键按下的事件
-        document.addEventListener('keydown',this.keydownHandler);
+        document.addEventListener('keydown', this.keydownHandler);
         // 调用run方法，使蛇移动
         this.run();
     }
 
-    keydownHandler=(event:KeyboardEvent)=>{
-        this.direction=event.key;
+    keydownHandler = (event: KeyboardEvent) => {
+        this.direction = event.key;
     }
 
-    run=()=>{
+    run = () => {
 
-        let X=this.snake.X;
-        let Y=this.snake.Y;
+        let X = this.snake.X;
+        let Y = this.snake.Y;
 
         switch (this.direction) {
             case "ArrowUp":
@@ -62,27 +64,37 @@ export default class GameControl {
                 break;
         }
 
-        this.snake.X=X;
-        this.snake.Y=Y;
+        // 检查蛇是否吃到了食物
+        this.checkEatFood(X, Y);
 
-        this.checkEatFood(X,Y);
+        try {
+            this.snake.X = X;
+            this.snake.Y = Y;
+        } catch (e) {
+            // 进入到catch，说明出现了异常，游戏结束，弹出一个提示信息
+            alert(e.message + ' GAME OVER!');
+            // 将isLive设置为false
+            this.isLive = false;
+        }
 
-        setTimeout(this.run,300-(this.scorePanel.level-1)*30);
+
+        this.isLive && setTimeout(this.run, 300 - (this.scorePanel.level - 1) * 30);
 
     }
 
-    checkEatFood=(X:number,Y:number)=>{
+    checkEatFood = (X: number, Y: number) => {
 
-        if (this.food.X===X&&this.food.Y===Y){
+        if (this.food.X === X && this.food.Y === Y) {
             // 分数增加
             this.scorePanel.addScore();
             // 食物的位置要进行重置
             this.food.change();
+            // 蛇的身体增加一节
+            this.snake.addBody();
         }
 
 
     }
-
 
 
 }
--- src/mouduls/Snake.ts
+++ src/mouduls/Snake.ts
@@ -27,12 +27,72 @@ export default class Snake {
     }
 
     set X(value:number){
+
+        // 如果新值和旧值相同，则直接返回不再修改
+        if(this.X === value){
+            return;
+        }
+        // X的值的合法范围0-290之间
+        if(value < 0 || value > 290){
+            // 进入判断说明蛇撞墙了
+            throw new Error('蛇撞墙了！');
+        }
+
+        // if (this.bodies[1]){
+        //
+        // }
+
+        this.moveBody();
         this.head.style.left=value+'px';
     }
 
     set Y(value:number){
+
+        // 如果新值和旧值相同，则直接返回不再修改
+        if(this.Y === value){
+            return;
+        }
+        // Y的值的合法范围0-290之间
+        if(value < 0 || value > 290){
+            // 进入判断说明蛇撞墙了
+            throw new Error('蛇撞墙了！');
+        }
+
+        this.moveBody();
         this.head.style.top=value+'px';
     }
+
+    addBody(){
+        // 向element中添加一个div beforeend标签结束之前
+        this.element.insertAdjacentHTML("beforeend", "<div></div>");
+    }
+
+    // 添加一个蛇身体移动的方法
+    moveBody(){
+
+        /*
+        *   将后边的身体设置为前边身体的位置
+        *       举例子：
+        *           第4节 = 第3节的位置
+        *           第3节 = 第2节的位置
+        *           第2节 = 蛇头的位置
+        * */
+        // 遍历获取所有的身体
+        for (let i=this.bodies.length-1;i>0;i--){
+
+            // 获取前边身体的位置
+            let X=(this.bodies[i-1] as HTMLElement).offsetLeft;
+            let Y=(this.bodies[i-1] as HTMLElement).offsetTop;
+            console.log(X);
+
+            // 将值设置到当前身体上
+            (this.bodies[i] as HTMLElement).style.left=X+'px';
+            (this.bodies[i] as HTMLElement).style.top=Y+'px';
+        }
+
+    }
+
+
 
 
 }
